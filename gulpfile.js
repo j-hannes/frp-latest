736d50550d9a80a8b31d7a1dc12cfd64957aaa19
J-Hannes <Hannes_E@gmx.de>
2015-02-17 14:58:08 +0100

    Use a basic gulp set up with stylus and browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,47 @@
 var gulp = require('gulp')
 var connect = require('gulp-connect')
 var watch = require('gulp-watch')
+var stylus = require('gulp-stylus')
+var plumber = require('gulp-plumber')
+var gutil = require('gulp-util')
+var watchify = require('watchify')
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
 
-gulp.task('server', function() {
+var onError = function (err) {
+  gutil.beep()
+  gutil.log(err)
+}
+
+var bundler = watchify(browserify('./dev/js/app.js', watchify.args))
+
+var bundle = function() {
+  return bundler.bundle()
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('build'))
+}
+
+bundler.on('update', bundle)
+
+var style = function() {
+  gulp.src('dev/css/main.styl')
+    .pipe(watch('dev/css/**/*.styl'))
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(stylus())
+    .pipe(gulp.dest('build'))
+}
+
+var connectServer = function() {
   connect.server({
-    root: 'dist',
+    root: 'build',
     livereload: true,
   })
-})
+  watch('build/*').pipe(connect.reload())
+}
 
-gulp.task('watch', function() {
-  watch('./dist/*').pipe(connect.reload())
-})
-
-gulp.task('default', ['server', 'watch'])
+gulp.task('js', bundle)
+gulp.task('css', style)
+gulp.task('server', connectServer)
+gulp.task('dev', ['js', 'css', 'server'])
 
